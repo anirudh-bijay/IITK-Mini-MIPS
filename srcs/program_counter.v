42962cd7039385de7ba54c31362d2cabac0c299b
anirudh-bijay <190929747+anirudh-bijay@users.noreply.github.com>
2025-03-29 22:57:16 +0530

    Create wrapper for program counter around (D) flip-flops

---
+++ srcs/program_counter.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: IIT Kanpur
+// Engineer: Anirudh Cheriyachanaseri Bijay
+// 
+// Create Date: 29.03.2025 18:24:52
+// Design Name: 
+// Module Name: program_counter
+// Project Name: IITK-Mini-MIPS
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// Wrapper around a register modelling D flip-flops to store the program
+// counter.
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module program_counter #(
+    parameter BUS_WIDTH = 9
+)(
+    input [BUS_WIDTH - 1 : 0] D,
+    input clk,
+    output reg [BUS_WIDTH - 1 : 0] Q
+);
+    always @(posedge clk)
+        Q <= D;
+endmodule
